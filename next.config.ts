f5efa19e5645fc2de1cda8a2336dd205dfe5e325
nivo0616 <nikita.vorobev@netcracker.com>
2025-04-10 20:18:06 +0300

    sitemap

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable' // 1 год
+          }
+        ]
+      },
+      {
+        source: '/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable' // 1 год
+          }
+        ]
+      },
+      {
+        source: '/(.*).(jpg|jpeg|png|gif|webp|svg|ico)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable' // 1 год
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
